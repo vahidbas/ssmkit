2f258770aa940519c69a0e343c636cc3b629aca7
Vahid Bastani <bastani.vahid@gmail.com>
2016-04-18 17:31:20 +0200

    some refactoring on hierarchy

--- src/ssmpack/process/hierachy.hpp
+++ src/ssmpack/process/hierachy.hpp
@@ -4,8 +4,46 @@
 
 #include <tao/seq/make_integer_range.hpp> //https://github.com/taocpp/sequences
 
+#include <vector>
+#include <algorithm>
+
 namespace ssmpack {
 namespace process {
+
+namespace detail {
+template <size_t N, size_t NA, size_t D, typename TArities>
+struct GetRandom {
+  template <typename TRV, typename TP, typename TA, size_t... Is>
+  static void apply(std::index_sequence<Is...>, TRV &rvs, TP &prc, TA args) {
+    std::get<N>(rvs) =
+        std::get<N>(prc).random(std::get<N - 1>(rvs), std::get<Is>(args)...);
+    constexpr size_t arity =
+        std::tuple_element<N + 1, TArities>::type::value - 1;
+    GetRandom<N + 1, NA + arity, D, TArities>::apply(
+        tao::seq::make_index_range<NA, NA + arity>(), rvs, prc, args);
+  }
+};
+
+template <size_t NA, size_t D, typename TArities>
+struct GetRandom<0, NA, D, TArities> {
+  template <typename TRV, typename TP, typename TA, size_t... Is>
+  static void apply(std::index_sequence<Is...>, TRV &rvs, TP &prc, TA args) {
+    std::get<0>(rvs) = std::get<0>(prc).random(std::get<Is>(args)...);
+    constexpr size_t arity = std::tuple_element<1, TArities>::type::value - 1;
+    GetRandom<1, NA + arity, D, TArities>::apply(
+        tao::seq::make_index_range<NA, NA + arity>(), rvs, prc, args);
+  }
+};
+
+template <size_t NA, size_t D, typename TArities>
+struct GetRandom<D, NA, D, TArities> {
+  template <typename TRV, typename TP, typename TA, size_t... Is>
+  static void apply(std::index_sequence<Is...>, TRV &rvs, TP &prc, TA args) {
+    std::get<D>(rvs) =
+        std::get<D>(prc).random(std::get<D - 1>(rvs), std::get<Is>(args)...);
+  }
+};
+} // namespace detail
 
 template <typename... Args>
 struct Hierachy {
@@ -21,63 +59,23 @@ struct Hierachy {
   TRandomVARs random(TVARs... args) {
     TRandomVARs a;
     constexpr size_t arity = std::tuple_element<0, TArities>::type::value;
-    functiontest<0, arity>::apply(
+    detail::GetRandom<0, arity, depth, TArities>::apply(
         tao::seq::make_index_range<0, arity>(), a, processes_,
-        std::make_tuple<args...>);
+        std::make_tuple(args...));
     return a;
   }
 
-  //  template <typename F, typename... A, size_t... Is>
-  //  TRandomVARs for_each(std::index_sequence<Is...>, F &&f, A... args) {
-  //    return {f(std::get<Is>(processes_), args...)...};
-  //  }
+  template<typename... TVARs>
+  void random_n(std::vector<TRandomVARs> samples, size_t n, TVARs... args)
+  {
+    samples.clear();
+    samples.resize(n);
+
+  }
 
   Hierachy(Args... processes) : processes_{processes...} {}
 
-  template <size_t N, size_t NA>
-  struct functiontest {
-    template <typename A, size_t... Is>
-    static void apply(std::index_sequence<Is...>, TRandomVARs &rvs,
-                      std::tuple<Args...> &prc, A args) {
-      std::get<N>(rvs) =
-          std::get<N>(prc).random(std::get<N - 1>(rvs), std::get<Is>(args)...);
-      constexpr size_t arity =
-          std::tuple_element<N + 1, TArities>::type::value-1;
-      functiontest<N + 1, NA + arity>::apply(
-          tao::seq::make_index_range<NA, NA + arity>(), rvs, prc, args);
-    }
-  };
-
-  template <size_t NA>
-  struct functiontest<0, NA> {
-    template <typename A, size_t... Is>
-    static void apply(std::index_sequence<Is...>, TRandomVARs &rvs,
-                      std::tuple<Args...> &prc, A args) {
-
-      std::get<0>(rvs) = std::get<0>(prc).random(std::get<Is>(args)...);
-
-      constexpr size_t arity = std::tuple_element<1, TArities>::type::value-1;
-
-      functiontest<1, NA + arity>::apply(
-          tao::seq::make_index_range<NA, NA + arity>(), rvs, prc, args);
-
-
-     // int b = std::index_sequence<Is...>::aa;
-    }
-  };
-
-  template <size_t NA>
-  struct functiontest<depth, NA> {
-    template <typename A, size_t... Is>
-    static void apply(std::index_sequence<Is...>, TRandomVARs &rvs,
-                      std::tuple<Args...> &prc, A args) {
-      std::get<depth>(rvs) = std::get<depth>(prc).random(
-         std::get<depth - 1>(rvs), std::get<Is>(args)...);
-      //int a = std::index_sequence<Is...>::aa;
-    }
-  };
-
   std::tuple<Args...> processes_;
 };
-}
-}
+} // namespace process
+} // namespace ssmpack
--- test/markov_test.cpp
+++ test/markov_test.cpp
@@ -14,40 +14,40 @@ using namespace PROJECT_NAME;
 BOOST_AUTO_TEST_SUITE(markov_test);
 
 BOOST_AUTO_TEST_CASE(test1) {
-arma::arma_rng::set_seed_random();
+  arma::arma_rng::set_seed_random();
 
-  model::LinearGaussian<2,2> f{{{1, 1}, {0, 1}}, {{0.0001, 0}, {0, 0.0001}}};
+  model::LinearGaussian<2, 2> f{{{1, 1}, {0, 1}}, {{0.0001, 0}, {0, 0.0001}}};
   distribution::Gaussian<2> nu;
 
-  model::LinearGaussian<1,2> h{{1, 0}, {0.0001}};
+  model::LinearGaussian<1, 2> h{{1, 0}, {0.0001}};
   distribution::Gaussian<1> omega;
 
-  distribution::Conditional<decltype(nu), decltype(f)> dyn(nu,f);
-  distribution::Conditional<decltype(omega), decltype(h)> obs(omega,h);
-  
+  distribution::Conditional<decltype(nu), decltype(f)> dyn(nu, f);
+  distribution::Conditional<decltype(omega), decltype(h)> obs(omega, h);
+
   process::Markov<decltype(dyn)> markov(dyn);
-process::Memoryless<decltype(obs)> memless(obs);
+  process::Memoryless<decltype(obs)> memless(obs);
 
-auto r = markov.random();
-auto r2 = memless.random(r);
-//double l = markov.likelihood(r);
+  auto r = markov.random();
+  auto r2 = memless.random(r);
+  // double l = markov.likelihood(r);
 
-process::Hierachy<decltype(markov), decltype(memless)> tt(markov, memless);
+  process::Hierachy<decltype(markov), decltype(memless)> tt(markov, memless);
 
- std::cout << process::ProcessTraits<decltype(markov)>::TArity::value <<
- std::endl;
+  std::cout << process::ProcessTraits<decltype(markov)>::TArity::value
+            << std::endl;
 
-auto r3 = tt.random();
+  auto r3 = tt.random(1, 'c');
 
-std::cout << std::get<0>(r3) <<  std::get<1>(r3) << std::endl;
-//  std::vector<typename decltype(markov_p)::TStateVAR> v;
-//  arma::arma_rng::set_seed_random();
-//  std::cout << markov_p.random(v, g, 10) << std::endl;
-//
-//  std::for_each(v.begin(), v.end(),
-//                [](typename decltype(markov_p)::TStateVAR &p) {
-//                  std::cout << p << std::endl;
-//                });
+  std::cout << std::get<0>(r3) << std::get<1>(r3) << std::endl;
+  //  std::vector<typename decltype(markov_p)::TStateVAR> v;
+  //  arma::arma_rng::set_seed_random();
+  //  std::cout << markov_p.random(v, g, 10) << std::endl;
+  //
+  //  std::for_each(v.begin(), v.end(),
+  //                [](typename decltype(markov_p)::TStateVAR &p) {
+  //                  std::cout << p << std::endl;
+  //                });
 }
 
 BOOST_AUTO_TEST_SUITE_END();
