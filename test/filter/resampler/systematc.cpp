5bf5745bc2e14dcf0855de7404cff23e68ffe933
Vahid Bastani <bastani.vahid@gmail.com>
2016-05-16 16:55:55 +0200

    implementing test cases for reampling algos

--- test/filter/resampler/systematc.cpp
+++ test/filter/resampler/systematc.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 #include "ssmpack/filter/resampler/systematic.hpp"
+#include <armadillo>
 
 using namespace ssmpack;
 
@@ -9,7 +10,24 @@ BOOST_AUTO_TEST_SUITE(filter_resampler_systematic);
 
 BOOST_AUTO_TEST_CASE(ordered_number_generator)
 {
+  struct AlwaysTrue {
+    bool operator()(arma::vec t) { return true; }
+  };
 
+  auto resampler = filter::resampler::makeSystematic(AlwaysTrue());
+  random::setRandomSeed();
+  int N = 500;
+  for (int i = 1; i < 100; ++i) {
+    auto u = resampler.generateOrderedNumbers(N);
+    // first element should be < 1/N
+    BOOST_CHECK(u[0] < 1.0 / N);
+    // all u elements should be in [0 1)
+    BOOST_CHECK(arma::all(u >= 0.0));
+    BOOST_CHECK(arma::all(u < 1.0));
+    // the difference between elements should be 1/N
+    BOOST_CHECK(arma::approx_equal(
+        arma::diff(u), arma::ones<arma::vec>(N-1) * 1.0 / N, "absdiff", 0.001));
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END();
