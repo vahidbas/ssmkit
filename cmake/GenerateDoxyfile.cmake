4605c57ffbaafb29af0aa84f16bd2e464f2fdf5a
Vahid Bastani <bastani.vahid@gmail.com>
2016-06-06 17:29:22 +0200

    cleaning up cmake directory

--- cmake/GenerateDoxyfile.cmake
+++ cmake/GenerateDoxyfile.cmake
@@ -1,35 +1,21 @@
-# We need to modify the Doxyfile slightly.  We'll copy the Doxyfile into the
-# build directory, update the location of the source, and then run Doxygen and
-# it will generate the documentation into the build directory.
+# Modifies and copies Doxyfile into build directory
 
-# First, read the Doxyfile in as a variable.
+# Read the Doxyfile into a variable.
 file(READ "${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile" DOXYFILE_CONTENTS)
 
-# Now, modify all the "INPUT" paths.  I've written each of the three out by
-# hand.  If more are added, they'll need to be added here too.
+# Modify all the input path
 string(REPLACE
     "./src/ssmpack"
     "${CMAKE_CURRENT_SOURCE_DIR}/src/ssmpack"
     DOXYFILE_AUXVAR "${DOXYFILE_CONTENTS}"
     )
-#message(status ${PROJECT_BINARY_DIR})
+# Modify documentation global path
 string(REPLACE
     "./doc/global"
     "${CMAKE_CURRENT_SOURCE_DIR}/doc/global"
     DOXYFILE_AUXVAR "${DOXYFILE_AUXVAR}"
     )
 
-#string(REPLACE
-#    "./doc/tutorials"
-#    "${CMAKE_CURRENT_SOURCE_DIR}/doc/tutorials"
-#    DOXYFILE_AUXVAR "${DOXYFILE_CONTENTS}"
-#    )
-#
-#string(REPLACE
-#    "./doc/policies"
-#    "${CMAKE_CURRENT_SOURCE_DIR}/doc/policies"
-#    DOXYFILE_CONTENTS "${DOXYFILE_AUXVAR}")
-#
 # Change the STRIP_FROM_PATH so that it works right even in the build directory;
 # otherwise, every file will have the full path in it.
 string(REGEX REPLACE
