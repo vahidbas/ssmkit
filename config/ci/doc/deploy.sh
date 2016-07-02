0ca54f4fc9261552988a8e30385be763aed78a15
Vahid Bastani <bastani.vahid@gmail.com>
2016-07-02 12:01:15 +0200

    auth issue 2

--- config/ci/doc/deploy.sh
+++ config/ci/doc/deploy.sh
@@ -12,8 +12,9 @@ ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
+# eval `ssh-agent -s`
+# ssh-add deploy_key
+cp deploy_key ~/.ssh/id_rsa
 cd ../../..
 
 git clone https://gitlab.com/vahid-bastani/ssmpack.git build/ssmpack
